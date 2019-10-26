2b523763a255c727b25058c8e1796b1032792e46
Vifinka <35996319+Vifinka@users.noreply.github.com>
2019-10-26 09:26:32 +0200

    17.7
    js fix

--- packs/index.js
+++ packs/index.js
@@ -1,6 +1,6 @@
 // all the variables
 
-var option = "<div class=\"level-background\" id=\"background\"><div class=\"level-top\"><div class=\"level-top-25\" id=\"top-left\"><img src=\"\" class=\"level-top-logo\" id=\"top-differ\"></div><div class=\"level-top-60\" id=\"top-center\"></div><div class=\"level-top-15\" id=\"top-right\"><img src=\"\" class=\"level-top-logo\" id=\"top-icon\"></div></div><div class=\"level-bottom\"><div class=\"level-bottom-inner\"><div class=\"level-bottom-inner-name\"><span class=\"delicious\">Info:</span></div><div class=\"level-bottom-inner-text\" id=\"info\"></div></div><div class=\"level-bottom-inner\"><div class=\"level-bottom-inner-list-name\"><span class=\"delicious\">Pomocky:</span></div><div class=\"level-bottom-inner-list-text\"><ul class=\"list-outside\" id=\"pomocky\"></ul></div><div class=\"level-bottom-inner-postup-text\"><ol class=\"postup-outside\" id=\"postup\"></ol></div><div class=\"level-bottom-inner-postup-name\"><span class=\"delicious\">Postup:</span></div></div></div></div>"
+var option = "<div class=\"level-background\" id=\"background\"><div class=\"level-top\"><div class=\"level-top-25\" id=\"top-left\"><img src=\"\" class=\"level-top-logo\" id=\"top-differ\"></div><div class=\"level-top-60\" id=\"top-center\"></div><div class=\"level-top-15\" id=\"top-right\"><img src=\"\" class=\"level-top-logo\" id=\"top-icon\"></div></div><div class=\"level-bottom\" id=\"level-bottom\"><div class=\"level-bottom-inner\"><div class=\"level-bottom-inner-name\"><span class=\"delicious\">Info:</span></div><div class=\"level-bottom-inner-text\" id=\"info\"></div></div><div class=\"level-bottom-inner\"><div class=\"level-bottom-inner-list-name\"><span class=\"delicious\">Pomocky:</span></div><div class=\"level-bottom-inner-list-text\"><ul class=\"list-outside\" id=\"pomocky\"></ul></div><div class=\"level-bottom-inner-postup-text\"><ol class=\"postup-outside\" id=\"postup\"></ol></div><div class=\"level-bottom-inner-postup-name\"><span class=\"delicious\">Postup:</span></div></div></div></div>"
 var image = ""
 var Packs = "";
 var video_div = "<iframe width=\"600\" height=\"360\" src=\"r\" id=\"level-vid\" class=\"level-video\"></iframe>";
@@ -46,7 +46,8 @@ function create_div(){
             document.getElementById("postup").id += "-" + i;
         
             if(Packs[i].video != "-"){
-                document.getElementById("background-" + i). innerHTML += video_div;
+                document.getElementById("level-bottom").innerHTML += video_div;
+                document.getElementById("level-bottom").id += "-" + i;
                 document.getElementById("level-vid").src = Packs[i].video;
                 document.getElementById("level-vid").id += "-" + i;
             }
