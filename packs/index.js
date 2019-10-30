c582022f7597c1ee28e8e849bb2e399b9f4e7fc5
Vifinka <35996319+Vifinka@users.noreply.github.com>
2019-10-30 15:01:15 +0100

    22.0
    js update

--- packs/index.js
+++ packs/index.js
@@ -1,9 +1,14 @@
 // all the variables
 
-var option = "<div class=\"level-background\" id=\"background\"><div class=\"level-top\"><div class=\"level-top-25\" id=\"top-left\"><img src=\"\" class=\"level-top-logo\" id=\"top-differ\"></div><div class=\"level-top-60\" id=\"top-center\"></div><div class=\"level-top-15\" id=\"top-right\"><img src=\"\" class=\"level-top-logo\" id=\"top-icon\"></div></div><div class=\"level-bottom\" id=\"level-bottom\"><div class=\"level-bottom-inner\"><div class=\"level-bottom-inner-name\"><span class=\"delicious\">Info:</span></div><div class=\"level-bottom-inner-text\" id=\"info\"></div></div><div class=\"level-bottom-inner\"><div class=\"level-bottom-inner-list-name\"><span class=\"delicious\">Pomocky:</span></div><div class=\"level-bottom-inner-list-text\"><ul class=\"list-outside\" id=\"pomocky\"></ul></div><div class=\"level-bottom-inner-postup-text\"><ol class=\"postup-outside\" id=\"postup\"></ol></div><div class=\"level-bottom-inner-postup-name\"><span class=\"delicious\">Postup:</span></div></div></div></div>"
+var option = "<div class=\"level-background\" id=\"background\"><div class=\"level-top\"><div class=\"level-top-25\" id=\"top-left\"><img src=\"\" class=\"level-top-logo\" id=\"top-differ\"></div><div class=\"level-top-60\" id=\"top-center\"></div><div class=\"level-top-15\" id=\"top-right\"><img src=\"\" class=\"level-top-logo\" id=\"top-icon\"></div></div>"
+var info_div = "<div class=\"level-bottom-inner\"><div class=\"level-bottom-inner-name\"><span class=\"delicious\">Info:</span></div><div class=\"level-bottom-inner-text\" id=\"info\"></div></div>"
+var pomocky_div = "<div class=\"level-bottom-inner-list-name\"><span class=\"delicious\">Pomocky:</span></div><div class=\"level-bottom-inner-list-text\"><ul class=\"list-outside\" id=\"pomocky\"></ul></div>";
+var postup_div = "<div class=\"level-bottom-inner-postup-text\"><ol class=\"postup-outside\" id=\"postup\"></ol></div><div class=\"level-bottom-inner-postup-name\"><span class=\"delicious\">Postup:</span></div></div>";
 var image = ""
 var Packs = "";
 var video_div = "<div class=\"level-video-outside\"><iframe width=\"600\" height=\"360\" src=\"r\" id=\"level-vid\" class=\"level-video\"></iframe></div>";
+
+
 var mentioned = 0;
 
 
@@ -17,6 +22,8 @@ function create_div(){
     for(var i = 0; i < Packs.length; i++){
         if(choosen == Packs[i].pack_name){
             mentioned ++;
+
+            // top settings
             document.getElementById("THE_DIV").innerHTML += option;
     
             document.getElementById("background").id += "-" + i;
@@ -35,25 +42,50 @@ function create_div(){
     
             document.getElementById("top-icon").src = Packs[i].type_icon;
             document.getElementById("top-icon").id += "-" + i;
-        
-            document.getElementById("info").innerHTML = Packs[i].info;
-            document.getElementById("info").id += "-" + i;
-        
-            document.getElementById("pomocky").innerHTML = Packs[i].pomocky;
-            document.getElementById("pomocky").id += "-" + i;
-        
-            document.getElementById("postup").innerHTML = Packs[i].postup;
-            document.getElementById("postup").id += "-" + i;
-        
+
+            // the checking settings
+            
+
+            document.getElementById("level-bottom").id += "-" + i;
+
+            if(Packs[i].info != "-"){
+                document.getElementById("level-bottom" + "-" + i).innerHTML += info_div;
+                document.getElementById("info").innerHTML = Packs[i].info;
+                document.getElementById("info").id += "-" + i;
+            }
+
+
+            if(Packs[i].pomocky != "-" || Packs[i].postup != "-"){
+
+                document.getElementById("level-bottom" + "-" + i).innerHTML = "<div class=\"level-bottom-inner\">";
+
+                if(Packs[i].pomocky != "-"){
+                    document.getElementById("level-bottom" + "-" + i).innerHTML += info_pomocky;
+                    document.getElementById("pomocky").innerHTML = Packs[i].pomocky;
+                    document.getElementById("pomocky").id += "-" + i;
+    
+                    
+                }
+    
+                if(Packs[i].postup != "-"){
+                    document.getElementById("level-bottom" + "-" + i).innerHTML += info_postup;
+                    document.getElementById("postup").innerHTML = Packs[i].postup;
+                    document.getElementById("postup").id += "-" + i;
+                }
+            
+                document.getElementById("level-bottom" + "-" + i).innerHTML += "</div>";    
+            
+            }
+
+            document.getElementById("level-bottom" + "-" + i).innerHTML += "</div>";    
+
+
+
             if(Packs[i].video != "-"){
-                document.getElementById("level-bottom").innerHTML += video_div;
-                document.getElementById("level-bottom").id += "-" + i;
+                document.getElementById("level-bottom" + "-" + i).innerHTML += video_div;
                 document.getElementById("level-vid").src = Packs[i].video;
                 document.getElementById("level-vid").id += "-" + i;
             }
-            // if(Packs[i].image_1 != "-"){
-            //     document.getElementById("background-" + i).innerHTML += image;
-            // }
         }
        
     }
