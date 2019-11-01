fff196d54c73bb94e449f6a097b81d290c2173ff
Vifinka <35996319+Vifinka@users.noreply.github.com>
2019-11-01 18:54:31 +0100

    22.9
    js edit

--- packs/index.js
+++ packs/index.js
@@ -59,7 +59,7 @@ function create_div(){
             if(Packs[i].pomocky != "-" || Packs[i].postup != "-"){
 
                 document.getElementById("level-bottom" + "-" + i).innerHTML += "<div class=\"level-bottom-inner\"></div>";
-                document.getElementsByClassName("level-bottom-inner")[1].id = "level-bottom-inner" + i;
+                document.getElementsByClassName("level-bottom-inner")[1].id = "level-bottom-inner-" + i;
 
                 if(Packs[i].pomocky != "-"){
                     document.getElementById("level-bottom-inner" + "-" + i).innerHTML += pomocky_div;
