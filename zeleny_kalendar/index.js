f082dd668e4e6b56c0829a799bfd391b12bf90e7
Vifinka <35996319+Vifinka@users.noreply.github.com>
2019-12-28 18:12:26 +0100

    31.8
    k so i didnt really work, so im trying a smaller if statement

--- zeleny_kalendar/index.js
+++ zeleny_kalendar/index.js
@@ -57,7 +57,7 @@
     document.getElementById("day").innerHTML = i + 1;
   
     for(var l = 0; l < Events.length; l++){
-      if((i + 1) == Events[l].day && month_num + 1 == Events[l].month && year == Events[l].year){
+      if((i + 1) == Events[l].day){
       document.getElementById("day").style.backgroundColor = "background-color:rgb(174, 255, 167);";
       console.log("yes");
       create_event(Events[l].day, Events[l].month, Events[l].year, Events[l].org, Events[l].name, Events[l].link, Events[l].text, l)
